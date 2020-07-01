9164c4a9f040b7b95bb3f2b790a67b64608a2a72
Ильнур Ибрагимов <ilnuribragimov@Mac-mini-Ilnur.local>
2020-07-01 15:28:10 +0200

    initial commit

---
+++ src/components/todoList/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+
+const TodoList = () => {
+  return (
+    <Container>
+      <Button> Hello</Button>
+    </Container>
+  );
+};
+
+export default TodoList;
