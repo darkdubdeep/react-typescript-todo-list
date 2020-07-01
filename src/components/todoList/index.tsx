63b1af5fcdfe12cbe7218a9b1466e86a5bd9d056
Ильнур Ибрагимов <ilnuribragimov@Mac-mini-Ilnur.local>
2020-07-01 22:26:40 +0200

    add filtering and checkbox logic

--- src/components/todoList/index.tsx
+++ src/components/todoList/index.tsx
@@ -12,31 +12,72 @@ import FormControl from 'react-bootstrap/FormControl';
 type Todo = { completed: boolean; id: number; title: string; userId: number };
 
 const TodoList = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [initialValues, setInitialValues] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>(initialValues);
+  const [filter, setFilter] = useState('all');
+  const [completedTodo, setCompletedTodo] = useState<Number>();
 
   useEffect(() => {
     async function fetchTodos() {
       let response = await fetch('https://jsonplaceholder.typicode.com/todos');
       let data = await response.json();
       //imitate results=10 and set result of fetching to the state
-      setTodos(data.slice(0, 10));
+      const todos = data.slice(0, 10);
+      setInitialValues(todos);
+      window.localStorage.setItem('todos', JSON.stringify(todos));
     }
-    fetchTodos();
+    const initialValues = JSON.parse(window.localStorage.getItem('todos')!);
+    if (initialValues) {
+      setInitialValues(initialValues);
+    } else {
+      fetchTodos();
+    }
   }, []);
+  useEffect(() => {
+    /*Filtering todos*/
+    if (filter === 'all') {
+      setTodos(initialValues);
+    } else if (filter === 'completed') {
+      setTodos(initialValues.filter((item) => item.completed === true));
+    } else {
+      setTodos(initialValues.filter((item) => item.completed === false));
+    }
+  }, [filter, initialValues]);
+
+  /*set complete status of todo*/
+  const changetodoStatus = (event: any, id: number) => {
+    const foundIndex = initialValues.findIndex((item) => item.id === id);
+    const todosToChange = JSON.parse(JSON.stringify(initialValues));
+    todosToChange[foundIndex].completed = event.target.checked;
+    setInitialValues(todosToChange);
+  };
   return (
     <Container>
       <DropdownButton
         variant="outline-secondary"
-        title="Sort by"
+        title="Show"
         id="input-group-dropdown-1"
       >
-        <Dropdown.Item href="#">All</Dropdown.Item>
-        <Dropdown.Item href="#">Completed</Dropdown.Item>
-        <Dropdown.Item href="#">Uncompleted</Dropdown.Item>
+        <Dropdown.Item href="#" onClick={() => setFilter('all')}>
+          All
+        </Dropdown.Item>
+        <Dropdown.Item href="#" onClick={() => setFilter('completed')}>
+          Completed
+        </Dropdown.Item>
+        <Dropdown.Item href="#" onClick={() => setFilter('uncompleted')}>
+          Uncompleted
+        </Dropdown.Item>
       </DropdownButton>
       <ListGroup>
         {todos.map((item) => (
-          <ListGroup.Item key={item.id}>{item.title}</ListGroup.Item>
+          <ListGroup.Item key={item.id}>
+            <InputGroup.Checkbox
+              aria-label="todo status checkbox"
+              checked={item.completed}
+              onChange={(event) => changetodoStatus(event, item.id)}
+            />
+            {item.title}
+          </ListGroup.Item>
         ))}
       </ListGroup>
       <InputGroup>
